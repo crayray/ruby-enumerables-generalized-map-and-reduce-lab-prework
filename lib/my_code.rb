0a1cde51b71e12e821cbfc6d01a1f3e34c5be1c5
Rachel Fuerst <rach.fuerst@gmail.com>
2019-10-23 15:29:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,7 +15,7 @@ def reduce(source_array, starting_point=nil)
   else
     sum = source_array[0]
     i = 1
-
+  end
     while i < source_array.length
       sum = yield(sum,source_array[i])
       i += 0
