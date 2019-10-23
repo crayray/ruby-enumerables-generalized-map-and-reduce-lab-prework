cc5c0684517c2a317e5a792b35134ec729a494da
Rachel Fuerst <rach.fuerst@gmail.com>
2019-10-23 14:37:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,31 @@
-# Your Code Here
+def map(array)
+  i = 0
+  new_array = []
+  while i < array.length
+    new_array.push(yield array[i])
+    i = i + 1
+  end
+  new_array
+end
+
+def reduce(source_array, *starting_point)
+  i = 0
+  # reduced_value = ""
+  # sum = 0
+  while i < source_array.length
+    # new_array.push(yield source_array[i])
+    yield source_array[i]
+    i = i + 1
+  end
+  print source_array
+  # print reduced_value
+end
+# def map(array)
+#   yeild(array)
+# end
+
+# map(element1) {}
+# def map(element1)
+#   element1.each
+#   yield element1
+# end
